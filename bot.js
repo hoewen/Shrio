2055fc52dc47166359d7e022b995b1ca27e8dc47
hoewen <37493791+hoewen@users.noreply.github.com>
2018-03-18 13:18:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
+     if (message.author.bot) return;
      if(message.content.toLowerCase().includes("galra"))
                message.channel.send("They will be defeated, don't you worry. The galaxy will soon be safe.")
 });
