26e049e8016d80cbcf32a4f79d8ad97245f5ea85
hoewen <37493791+hoewen@users.noreply.github.com>
2018-03-18 20:34:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,5 +7,12 @@ client.on('message', message => {
                message.channel.send("They will be defeated, don't you worry. The galaxy will soon be safe.")
 });
 
+client.on('message', message => {
+     if (message.author.bot) return;
+     if(message.content.toLowerCase().includes("space daddy"))
+               message.channel.send("I appreciate you look up to me as a father, but I think 'daddy' is a bit... inappropriate.")
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
